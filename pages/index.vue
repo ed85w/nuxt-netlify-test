e47cd372006e7a81647d3f25e552105d784a6e56
ed85w <ed.walker@hotmail.co.uk>
2021-02-10 21:18:21 +0000

    app.html added to root

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="container">
     <h1>hellozz</h1>
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+<form 
+  name="contactus" 
+  action="/thanks" 
+  method="post" 
+  netlify
+  netlify-honeypot="bot-field"
+>
+  <!-- // Hidden input to check for bots -->
+  <input type="hidden" name="form-name" value="contactus" />
+  <div>  
+    <label for="name">Name:</label> 
+    <input type="text" name="name" required/>
+  </div>
+  <div>
+    <input type="email" name="email" required/>
+    <label for="email">Email:</label>
+  </div>
+  <div>
+    <textarea name="message" required></textarea>
+    <label for="message">Message:</label>
+  </div>
+  <button type="submit" value="Send message">Send</button>
 </form>
     <div>
       <Logo />
