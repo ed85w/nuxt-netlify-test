5a8f7d66da37bd297d6d46f566651be73d16f9d2
ed85w <ed.walker@hotmail.co.uk>
2021-02-10 21:03:38 +0000

    test form added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="container">
-    <h1>hello</h1>
+    <h1>hellozz</h1>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     <div>
       <Logo />
       <h1 class="title">
